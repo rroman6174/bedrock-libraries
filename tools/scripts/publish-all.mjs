3268f1b90cbd528b899ac49f36184ed1b42a8600
rroman6174@gmail.com <rroman6174@gmail.com>
2024-02-01 15:47:32 -0500

    test

---
+++ tools/scripts/publish-all.mjs
@@ -0,0 +1,22 @@
+import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
+import path from 'path';
+import { glob } from 'glob-promise';
+import { publishVersion } from './publish.mjs';
+import * as packageJson from '../../package.json';
+const parse = JSON.parse;
+const stringify = JSON.stringify;
+// Update root package.json version
+// execSync('npm version patch', { stdio: 'inherit' });
+
+// Assuming your libraries are located under 'libs' directory
+const libraries = glob.glob.sync('libs/*');
+const [, , otp] = process.argv;
+if (!otp) {
+  console.error('No OTP token provided.');
+  process.exit(1);
+}
+
+for (const library of libraries) {
+  publishVersion(path.basename(library), otp);
+}
--- tools/scripts/publish.mjs
+++ tools/scripts/publish.mjs
@@ -20,41 +20,39 @@ function invariant(condition, message) {
   }
 }
 
-// Executing publish script: node path/to/publish.mjs {name} --version {version} --tag {tag}
-// Default "tag" to "next" so we won't publish the "latest" tag by accident.
-const [, , name, version, otp, tag = 'next'] = process.argv;
+export function publishVersion(name, otp, tag = 'next') {
+  // A simple SemVer validation to validate the version
+  const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
+  invariant(
+    version && validVersion.test(version),
+    `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
+  );
 
-// A simple SemVer validation to validate the version
-const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
-invariant(
-  version && validVersion.test(version),
-  `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
-);
+  const graph = readCachedProjectGraph();
+  const project = graph.nodes[name];
 
-const graph = readCachedProjectGraph();
-const project = graph.nodes[name];
+  invariant(
+    project,
+    `Could not find project "${name}" in the workspace. Is the project.json configured correctly?`
+  );
 
-invariant(
-  project,
-  `Could not find project "${name}" in the workspace. Is the project.json configured correctly?`
-);
+  const outputPath = project.data?.targets?.build?.options?.outputPath;
+  invariant(
+    outputPath,
+    `Could not find "build.options.outputPath" of project "${name}". Is project.json configured  correctly?`
+  );
 
-const outputPath = project.data?.targets?.build?.options?.outputPath;
-invariant(
-  outputPath,
-  `Could not find "build.options.outputPath" of project "${name}". Is project.json configured  correctly?`
-);
+  process.chdir(outputPath);
 
-process.chdir(outputPath);
+  // Updating the version in "package.json" before publishing
+  try {
+    const json = JSON.parse(readFileSync(`package.json`).toString());
+    json.version = version;
+    writeFileSync(`package.json`, JSON.stringify(json, null, 2));
+  } catch (e) {
+    console.error(`Error reading package.json file from library build output.`);
+  }
 
-// Updating the version in "package.json" before publishing
-try {
-  const json = JSON.parse(readFileSync(`package.json`).toString());
-  json.version = version;
-  writeFileSync(`package.json`, JSON.stringify(json, null, 2));
-} catch (e) {
-  console.error(`Error reading package.json file from library build output.`);
+  // Execute "npm publish" to publish
+  execSync(`npm publish --access public --tag ${tag} --otp ${otp}`);
 }
-
-// Execute "npm publish" to publish
-execSync(`npm publish --access public --tag ${tag}`);
